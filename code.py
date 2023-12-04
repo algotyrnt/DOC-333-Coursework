af7d78d2c44e4070a9e33b7af3fe2fc8d28e4ff9
algotyrnt <punjitha@gmail.com>
2023-12-04 07:29:54 +0530

    process, gui, error handeling upgrade

--- code.py
+++ code.py
@@ -41,7 +41,6 @@ def addProject():
     addproject = Toplevel(mainmenu)
     addproject.geometry("700x370")
     addproject.title("Add a new project")
-
     
     pc = IntVar()
     cn = StringVar()
@@ -65,7 +64,6 @@ def addProject():
     dash4 = Label(addproject, text="-", font=("arial",10)).place(x=250,y=190)
     dash5 = Label(addproject, text="-", font=("arial",10)).place(x=250,y=220)
     dash6 = Label(addproject, text="-", font=("arial",10)).place(x=250,y=250)
-
 
     entry1 = Entry(addproject, textvariable = pc).place(x=525,y=100)
     entry2 = Entry(addproject, textvariable = cn).place(x=525,y=130)
@@ -103,83 +101,88 @@ def addProject():
             if (ProjectCode == 0):
                 addproject.destroy()
             else:
+                StartDate = sd.get()
+                ExEndDate = ed.get()
+                NumberOfWorkers = nw.get()
                 try:
                     ClientsName = cn.get()
-                    StartDate = sd.get()
-                    ExEndDate = ed.get()
-                    NumberOfWorkers = nw.get()
                     ProjectStat = listbox1.get(stat[0])
                 except:
                     messagebox.showerror("error", "all fields are required")
                     addproject.destroy()
                     addProject()
                 else:
-                    if(ProjectCode > 0 and NumberOfWorkers > 0):
-                        result = messagebox.askyesno('Save details', 'Do you want to save the project')
-                        if result:
-                            if(len(ProjectsDetails) == 0):
-                                if(ProjectStat == "completed"):
-                                    actualenddate = askstring("Actual end date", "Actual end date of the project")
-                                    ProjectDetails = [[ProjectCode, ClientsName, StartDate, ExEndDate, NumberOfWorkers, ProjectStat, actualenddate]]
-                                    ProjectsDetails.extend(ProjectDetails)
-                                    messagebox.showinfo("show info", "Project saved")
-                                    print(ProjectsDetails)
-                                    addproject.destroy()
-                                elif(ProjectStat == "on hold"):
-                                    ProjectDetails = [[ProjectCode, ClientsName, StartDate, ExEndDate, NumberOfWorkers, ProjectStat]]
-                                    ProjectsDetails.extend(ProjectDetails)
-                                    messagebox.showinfo("show info", "Project saved")
-                                    print(ProjectsDetails)
-                                    addproject.destroy()
-                                elif(ProjectStat == "ongoing" and NumberOfWorkers <= AvWorkers):
-                                    ProjectDetails = [[ProjectCode, ClientsName, StartDate, ExEndDate, NumberOfWorkers, ProjectStat]]
-                                    ProjectsDetails.extend(ProjectDetails)
-                                    AvWorkers -= NumberOfWorkers
-                                    messagebox.showinfo("show info", "Project saved")
-                                    print(ProjectsDetails)
-                                    addproject.destroy()
-                                else:
-                                    messagebox.showwarning("warning", "There is no enough workers")
-                                    addproject.destroy()
-                                    addProject()
-                            else:
-                                for index in range(len(ProjectsDetails)):
-                                    if ProjectCode in ProjectsDetails[index]:
-                                        messagebox.showwarning("warning", "Project Code allready exits")
+                    if(len(ClientsName) > 0 and len(StartDate) >0 and len(ExEndDate0) > 0):
+                        if(ProjectCode > 0 and NumberOfWorkers > 0):
+                            result = messagebox.askyesno('Save details', 'Do you want to save the project')
+                            if result:
+                                if(len(ProjectsDetails) == 0):
+                                    if(ProjectStat == "completed"):
+                                        actualenddate = askstring("Actual end date", "Actual end date of the project")
+                                        ProjectDetails = [[ProjectCode, ClientsName, StartDate, ExEndDate, NumberOfWorkers, ProjectStat, actualenddate]]
+                                        ProjectsDetails.extend(ProjectDetails)
+                                        messagebox.showinfo("show info", "Project saved")
+                                        print(ProjectsDetails)
+                                        addproject.destroy()
+                                    elif(ProjectStat == "on hold"):
+                                        ProjectDetails = [[ProjectCode, ClientsName, StartDate, ExEndDate, NumberOfWorkers, ProjectStat]]
+                                        ProjectsDetails.extend(ProjectDetails)
+                                        messagebox.showinfo("show info", "Project saved")
+                                        print(ProjectsDetails)
+                                        addproject.destroy()
+                                    elif(ProjectStat == "ongoing" and NumberOfWorkers <= AvWorkers):
+                                        ProjectDetails = [[ProjectCode, ClientsName, StartDate, ExEndDate, NumberOfWorkers, ProjectStat]]
+                                        ProjectsDetails.extend(ProjectDetails)
+                                        AvWorkers -= NumberOfWorkers
+                                        messagebox.showinfo("show info", "Project saved")
+                                        print(ProjectsDetails)
+                                        addproject.destroy()
+                                    else:
+                                        messagebox.showwarning("warning", "There is no enough workers")
                                         addproject.destroy()
                                         addProject()
-                                        break
-                                    else:
-                                        if(ProjectStat == "completed"):
-                                            actualenddate = askstring("Actual end date", "Actual end date of the project")
-                                            ProjectDetails = [[ProjectCode, ClientsName, StartDate, ExEndDate, NumberOfWorkers, ProjectStat, actualenddate]]
-                                            ProjectsDetails.extend(ProjectDetails)
-                                            messagebox.showinfo("show info", "Project saved")
-                                            print(ProjectsDetails)
-                                            addproject.destroy()
-                                        elif(ProjectStat == "on hold"):
-                                            ProjectDetails = [[ProjectCode, ClientsName, StartDate, ExEndDate, NumberOfWorkers, ProjectStat]]
-                                            ProjectsDetails.extend(ProjectDetails)
-                                            messagebox.showinfo("show info", "Project saved")
-                                            print(ProjectsDetails)
-                                            addproject.destroy()
-                                        elif(ProjectStat == "ongoing" and NumberOfWorkers <= AvWorkers):
-                                            ProjectDetails = [[ProjectCode, ClientsName, StartDate, ExEndDate, NumberOfWorkers, ProjectStat]]
-                                            ProjectsDetails.extend(ProjectDetails)
-                                            AvWorkers -= NumberOfWorkers
-                                            messagebox.showinfo("show info", "Project saved")
-                                            print(ProjectsDetails)
-                                            addproject.destroy()
-                                        else:
-                                            messagebox.showwarning("warning", "There is no enough workers")
+                                else:
+                                    for index in range(len(ProjectsDetails)):
+                                        if ProjectCode in ProjectsDetails[index]:
+                                            messagebox.showwarning("warning", "Project Code allready exits")
                                             addproject.destroy()
                                             addProject()
-                                        break
+                                            break
+                                        else:
+                                            if(ProjectStat == "completed"):
+                                                actualenddate = askstring("Actual end date", "Actual end date of the project")
+                                                ProjectDetails = [[ProjectCode, ClientsName, StartDate, ExEndDate, NumberOfWorkers, ProjectStat, actualenddate]]
+                                                ProjectsDetails.extend(ProjectDetails)
+                                                messagebox.showinfo("show info", "Project saved")
+                                                print(ProjectsDetails)
+                                                addproject.destroy()
+                                            elif(ProjectStat == "on hold"):
+                                                ProjectDetails = [[ProjectCode, ClientsName, StartDate, ExEndDate, NumberOfWorkers, ProjectStat]]
+                                                ProjectsDetails.extend(ProjectDetails)
+                                                messagebox.showinfo("show info", "Project saved")
+                                                print(ProjectsDetails)
+                                                addproject.destroy()
+                                            elif(ProjectStat == "ongoing" and NumberOfWorkers <= AvWorkers):
+                                                ProjectDetails = [[ProjectCode, ClientsName, StartDate, ExEndDate, NumberOfWorkers, ProjectStat]]
+                                                ProjectsDetails.extend(ProjectDetails)
+                                                AvWorkers -= NumberOfWorkers
+                                                messagebox.showinfo("show info", "Project saved")
+                                                print(ProjectsDetails)
+                                                addproject.destroy()
+                                            else:
+                                                messagebox.showwarning("warning", "There is no enough workers")
+                                                addproject.destroy()
+                                                addProject()
+                                            break
+                            else:
+                                addproject.destroy()
+                                addProject()
                         else:
+                            messagebox.showwarning("error", "Number of workers and project must be a valid counting number")
                             addproject.destroy()
                             addProject()
                     else:
-                        messagebox.showwarning("error", "Number of workers and project must be a valid counting number")
+                        messagebox.showerror("error", "all fields are required")
                         addproject.destroy()
                         addProject()
                 
@@ -196,8 +199,11 @@ def rProject():
 
     label1 = Label(rproject, text="XYZ Company", font=("arial",20,"bold", "italic")).pack()
     label2 = Label(rproject, text="Remove Completed Project", font=("arial",16)).pack()
-    label3 = Label(rproject, text="Project Code          -", font=("arial",10)).place(x=50,y=100)
+    label3 = Label(rproject, text="Project Code", font=("arial",10)).place(x=50,y=100)
+
     entry1 = Entry(rproject, textvariable = sb).place(x=525,y=100)
+
+    dash1 = Label(rproject, text="-", font=("arial",10)).place(x=250,y=100)
 
     def submitb():
         global ProjectsDetails
@@ -241,7 +247,7 @@ def rProject():
 def UpProject():
     upproject = Toplevel(mainmenu)
     upproject.geometry("700x400")
-    upproject.title("Add a new project")
+    upproject.title("Upate project details")
 
     pc = IntVar()
     cn = StringVar()
@@ -250,19 +256,29 @@ def UpProject():
     nw = IntVar()
 
     label1 = Label(upproject, text="XYZ Company", font=("arial",20,"bold", "italic")).pack()
-    label2 = Label(upproject, text="Add a new project", font=("arial",16)).pack()
-    label3 = Label(upproject, text="Project Code          -     **Enter '0' to Project Code to exit.", font=("arial",10)).place(x=50,y=100)
+    label2 = Label(upproject, text="Update Project Details", font=("arial",16)).pack()
+    label3 = Label(upproject, text="Project Code", font=("arial",10)).place(x=50,y=100)
+    label4 = Label(upproject, text="**Enter '0' to Project Code to exit.", font=("arial",10)).place(x=275,y=100)
+    label5 = Label(upproject, text="Clients Name", font=("arial",10)).place(x=50,y=130)
+    label6 = Label(upproject, text="Start date", font=("arial",10)).place(x=50,y=160)
+    label7 = Label(upproject, text="Expected end date", font=("arial",10)).place(x=50,y=190)
+    label8 = Label(upproject, text="Number of workers", font=("arial",10)).place(x=50,y=220)
+    label9 = Label(upproject, text="Project status", font=("arial",10)).place(x=50,y=250)
+
     entry1 = Entry(upproject, textvariable = pc).place(x=525,y=100)
-    label4 = Label(upproject, text="Clients Name         -", font=("arial",10)).place(x=50,y=130)
     entry2 = Entry(upproject, textvariable = cn).place(x=525,y=130)
-    label5 = Label(upproject, text="Start date              -", font=("arial",10)).place(x=50,y=160)
     entry3 = Entry(upproject, textvariable = sd).place(x=525,y=160)
-    label6 = Label(upproject, text="Expected end date -", font=("arial",10)).place(x=50,y=190)
     entry4 = Entry(upproject, textvariable = ed).place(x=525,y=190)
-    label7 = Label(upproject, text="Number of workers -", font=("arial",10)).place(x=50,y=220)
     entry5 = Entry(upproject, textvariable = nw).place(x=525,y=220)
-    label8 = Label(upproject, text="Project status        -", font=("arial",10)).place(x=50,y=250)
+
     listbox1 = Listbox(upproject,width=10,height=3, selectmode=SINGLE)
+
+    dash1 = Label(upproject, text="-", font=("arial",10)).place(x=250,y=100)
+    dash2 = Label(upproject, text="-", font=("arial",10)).place(x=250,y=130)
+    dash3 = Label(upproject, text="-", font=("arial",10)).place(x=250,y=160)
+    dash4 = Label(upproject, text="-", font=("arial",10)).place(x=250,y=190)
+    dash5 = Label(upproject, text="-", font=("arial",10)).place(x=250,y=220)
+    dash6 = Label(upproject, text="-", font=("arial",10)).place(x=250,y=250)
 
     listbox1.insert(1, "ongoing")
     listbox1.insert(2, "on hold")
@@ -271,7 +287,6 @@ def UpProject():
 
     def submitb():
         global ProjectsDetails
-        global cProjectsDetails
         global AvWorkers
         stat = listbox1.curselection()
         ProjectCode = 0
@@ -310,7 +325,7 @@ def UpProject():
                         if ProjectCode in ProjectsDetails[index]:
                             result = messagebox.askyesno('Update details', 'Do you want to update the project')
                             if result:
-                                projectStatus = ProjectDetails[index][5]
+                                projectStatus = ProjectsDetails[index][5]
                                 if(projectStatus == 'ongoing'):
                                     AvWorkers += ProjectsDetails[index][4]
                                 del ProjectsDetails[index]
@@ -383,7 +398,6 @@ def AddWorkers():
 #project statistics.
 def pStats():
     global ProjectsDetails
-    global cProjectsDetails
     global AvWorkers
     projectstat = Toplevel(mainmenu)
     projectstat.geometry("700x300")
@@ -392,23 +406,27 @@ def pStats():
     OP = 0
     HP = 0
     CP = 0
-    cP = cProjectsDetails.count(completed)
+
     for index in range(len(ProjectsDetails)):
         if ("ongoing" in ProjectsDetails[index]):
             OP += 1
         elif ("on hold" in ProjectsDetails[index]):
             HP += 1
+        elif ('completed' in ProjectsDetails[index]):
+            CP +=1
     
     label1 = Label(projectstat, text="XYZ Company", font=("arial",20,"bold", "italic")).pack()
     label2 = Label(projectstat, text="Project Statistics", font=("arial",16)).pack()
     label3 = Label(projectstat, text="Number of ongoing projects", font=("arial",10)).place(x=50,y=100)
-    dash1 = Label(projectstat, text="-", font=("arial",10)).place(x=300,y=100)
     label4 = Label(projectstat, text="Number of completed projects", font=("arial",10)).place(x=50,y=130)
-    dash2 = Label(projectstat, text="-", font=("arial",10)).place(x=300,y=130)
     label5 = Label(projectstat, text="Number of on hold projects", font=("arial",10)).place(x=50,y=160)
-    dash3 = Label(projectstat, text="-", font=("arial",10)).place(x=300,y=160)
     label6 = Label(projectstat, text="Number of available workers to assign", font=("arial",10)).place(x=50,y=190)
+
+    dash1 = Label(projectstat, text="-", font=("arial",10)).place(x=300,y=100)
+    dash2 = Label(projectstat, text="-", font=("arial",10)).place(x=300,y=130)
+    dash3 = Label(projectstat, text="-", font=("arial",10)).place(x=300,y=160)
     dash4 = Label(projectstat, text="-", font=("arial",10)).place(x=300,y=190)
+    
     data1 = Label(projectstat, text=OP, font=("arial",10)).place(x=525,y=100)
     data2 = Label(projectstat, text=CP, font=("arial",10)).place(x=525,y=130)
     data3 = Label(projectstat, text=HP, font=("arial",10)).place(x=525,y=160)
@@ -417,7 +435,8 @@ def pStats():
     label8 = Label(projectstat, text="Do you want to add a project", font=("arial",10)).place(x=50,y=250)
 
     def yeS():
-        projectstat()
+        projectstat.destroy()
+        addProject()
     def des():
         projectstat.destroy()
 
@@ -455,6 +474,6 @@ def submitb():
 
 entry1 = Entry(mainmenu, textvariable = yc).place(x=525,y=280)
 
-submit = Button(mainmenu, text="Submit Data", font=("arial",8), command=submitb).place(x=585,y=320)
+submit = Button(mainmenu, text="submit", font=("arial",8) , width=7, command=submitb).place(x=600,y=320)
 
 mainloop()
